205434a39a16cbdd241f9e01ae83d6fd623eb2d5
Samkiroko <kirokodev@gmail.com>
2020-06-20 02:59:08 +0300

    creating secret file

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -6,7 +6,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    storage :fog
+    storage :aws
   else
     storage :file
   end
